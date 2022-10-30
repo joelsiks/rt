f64ae2ddb5f22ca71d2ac3ebc97472c2efd3bb3f
Joel <joel.attano@gmail.com>
2022-10-30 09:46:18 +0100

    Reduced number of pixels to prevent crash.

--- src/main.cpp
+++ src/main.cpp
@@ -89,7 +89,7 @@ int main() {
 
     // Image
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 700;
+    const int image_width = 100;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 500;
     const int ray_bounce_limit = 30;
